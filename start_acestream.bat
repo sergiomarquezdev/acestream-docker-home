a66e6e06f8dada10759724d7d06e98e2c3b6202d
Sergio Márquez <marquezpsergio@gmail.com>
2024-04-07 01:32:06 +0200

    fix: typo

--- start_acestream.bat
+++ start_acestream.bat
@@ -6,26 +6,26 @@ set PORT=6878
 
 echo Verificando y procesando el argumento proporcionado...
 if "%~1"=="" (
-    echo Advertencia: No se proporcionó un ID de stream. Se abrirá la interfaz sin un stream predefinido.
+    echo Advertencia: No se proporciono un ID de stream. Se abrira la interfaz sin un stream predefinido.
     set "STREAM_ID="
 ) else (
     set "STREAM_ID=%~1"
-    REM Elimina el prefijo acestream:// si está presente y establece el ID del stream
+    REM Elimina el prefijo acestream:// si esta presente y establece el ID del stream
     set "STREAM_ID=%STREAM_ID:acestream://=%"
 )
-echo ID de stream a abrir: %STREAM_ID% (si está vacío, se omitirá el ID de stream)
+echo ID de stream a abrir: %STREAM_ID% (si esta vacio, se omitira el ID de stream)
 
-echo Verificando la instalación de Docker...
+echo Verificando la instalacion de Docker...
 docker --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo ERROR: Docker no está instalado. Instálalo para continuar.
+    echo ERROR: Docker no esta instalado. Instalalo para continuar.
     start https://www.docker.com/get-started/
     exit /b
 ) else (
     echo Docker instalado correctamente.
 )
 
-echo Descargando la última imagen de Docker desde el repositorio...
+echo Descargando la ultima imagen de Docker desde el repositorio...
 docker pull %IMAGE_NAME%
 
 echo Verificando la existencia del contenedor...
@@ -47,6 +47,6 @@ if not "%STREAM_ID%"=="" (
     echo Abriendo stream con ID: %STREAM_ID% en el navegador.
     start http://127.0.0.1:%PORT%/webui/player/%STREAM_ID%
 ) else (
-    echo No se especificó un ID de stream. Abriendo la interfaz web de Acestream sin un stream predefinido.
+    echo No se especifico un ID de stream. Abriendo la interfaz web de Acestream sin un stream predefinido.
     start http://127.0.0.1:%PORT%/webui/player/
 )
