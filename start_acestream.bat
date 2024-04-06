1022669331a32cc93277b9344ad96049c33f8b76
Sergio Márquez <marquezpsergio@gmail.com>
2024-04-07 01:30:59 +0200

    feat: process comments

--- start_acestream.bat
+++ start_acestream.bat
@@ -4,44 +4,49 @@ set IMAGE_NAME=smarquezp/docker-acestream-ubuntu:latest
 set CONTAINER_NAME=acestream-container
 set PORT=6878
 
-REM Verifica y procesa el argumento pasado
+echo Verificando y procesando el argumento proporcionado...
 if "%~1"=="" (
-    echo Ejecutado sin argumento.
+    echo Advertencia: No se proporcionó un ID de stream. Se abrirá la interfaz sin un stream predefinido.
     set "STREAM_ID="
 ) else (
     set "STREAM_ID=%~1"
-    REM Quita acestream:// del argumento pasado si está presente y lo setea en la variable
+    REM Elimina el prefijo acestream:// si está presente y establece el ID del stream
     set "STREAM_ID=%STREAM_ID:acestream://=%"
 )
-echo Abriendo stream ID: %STREAM_ID%
+echo ID de stream a abrir: %STREAM_ID% (si está vacío, se omitirá el ID de stream)
 
-REM Verifica que Docker esté instalado
+echo Verificando la instalación de Docker...
 docker --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Docker no está instalado. Por favor, instala Docker y vuelve a intentarlo.
+    echo ERROR: Docker no está instalado. Instálalo para continuar.
+    start https://www.docker.com/get-started/
     exit /b
+) else (
+    echo Docker instalado correctamente.
 )
 
-REM Descarga la última imagen desde el repositorio de Docker
+echo Descargando la última imagen de Docker desde el repositorio...
 docker pull %IMAGE_NAME%
 
-REM Verifica si el contenedor ya existe
+echo Verificando la existencia del contenedor...
 docker ps -a --filter "name=%CONTAINER_NAME%" --format "{{.Names}}" | findstr /c:"%CONTAINER_NAME%" >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Creando y ejecutando un nuevo contenedor...
+    echo Creando y ejecutando un nuevo contenedor basado en la imagen %IMAGE_NAME%...
 ) else (
-    echo Contenedor existente detectado. Deteniendo y eliminando...
+    echo Contenedor "%CONTAINER_NAME%" existente detectado. Deteniendo y eliminando para actualizar...
     docker rm -f %CONTAINER_NAME% >nul 2>&1
 )
+echo Ejecutando el contenedor "%CONTAINER_NAME%"...
 docker run -d -p %PORT%:%PORT% --name %CONTAINER_NAME% %IMAGE_NAME%
 
-REM Espera 3 segundos antes de abrir el navegador
-timeout /t 3 /nobreak > nul
+echo Esperando 5 segundos antes de abrir el navegador...
+timeout /t 5 /nobreak > nul
 
-REM Comprueba el STREAM_ID y abre el navegador en la URL específica
+echo Abriendo la interfaz web de Acestream...
 if not "%STREAM_ID%"=="" (
+    echo Abriendo stream con ID: %STREAM_ID% en el navegador.
     start http://127.0.0.1:%PORT%/webui/player/%STREAM_ID%
 ) else (
-    echo Advertencia: STREAM_ID no especificado. Abriendo el reproductor sin ID de stream.
+    echo No se especificó un ID de stream. Abriendo la interfaz web de Acestream sin un stream predefinido.
     start http://127.0.0.1:%PORT%/webui/player/
 )
