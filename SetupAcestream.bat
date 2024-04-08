ad480cb7e38cfbe7fe5e2b8aeb54056bcbb1134c
Sergio Márquez <marquezpsergio@gmail.com>
2024-04-08 02:27:52 +0200

    enhance: menu aesthetics and message clarity

--- SetupAcestream.bat
+++ SetupAcestream.bat
@@ -39,25 +39,27 @@ if "%STREAM_ID:~39,1%" NEQ "" if "%STREAM_ID:~40,1%"=="" (
 :menu
 set "OPTION="
 cls
-echo ==============================================================
-echo Bienvenido al asistente de configuracion de Acestream x Docker
-echo ==============================================================
+echo =======================================================================
+echo                            Acestream x Docker
+echo                       Asistente de Configuracion v2.2.0
+echo =======================================================================
 echo.
-echo Opciones disponibles:
-echo 1. Realizar instalacion de Docker (PRIMORDIAL)
-echo    Abre la web de Docker para que puedas realizar la instalacion.
-echo 2. Configurar el protocolo acestream en Windows (OPCIONAL)
-echo    Crea los registros necesarios en el Registro de Windows para que todos los enlaces 'acestream://xxx' se abran directamente con este script.
-echo 3. Instalar y ejecutar Acestream en Docker
-echo    Proceso de instalacion de Acestream en Docker (recuerda tener instalada y ejecutada la aplicacion Docker)
-echo 4. Ejecutar navegador con URL para Acestream
-echo    Si solamente quieres ejecutar el navegador con el enlace
-echo    (Si ya tienes corriendo el contenedor, si no, ejecuta el paso 3)
-echo 0. Salir
+echo [Menu de Opciones]
+echo -------------------------------------------------------------------------------
+echo [1] Instalacion de Docker (ESENCIAL)
+echo     > Abre la web de Docker para realizar la instalacion.
+echo [2] Configurar protocolo Acestream en Windows (OPCIONAL)
+echo    > Crea los registros necesarios en el Registro de Windows para abrir los enlaces 'acestream://' directamente.
+echo [3] Instalar y ejecutar Acestream en Docker
+echo    > Proceso de instalacion de Acestream en Docker (Docker debe estar en ejecucion).
+echo [4] Ejecutar navegador con URL para Acestream
+echo     > Abre el navegador con el enlace Acestream (requiere contenedor Docker corriendo y activo).
+echo [0] Salir
+echo -------------------------------------------------------------------------------
 echo.
-echo Introduce la opcion deseada y presiona ENTER. Para salir, introduce '0'.
+echo Selecciona una opcion y presiona ENTER. Para salir, introduce '0'.
 :: Solicita al usuario que introduzca el numero de la opcion que desea realizar.
-set /p OPTION="Opcion: "
+set /p OPTION="Opcion seleccionada: "
 
 :: Evalua la opcion introducida y redirige al bloque de codigo correspondiente.
 if "%OPTION%"=="0" goto end
@@ -69,72 +71,81 @@ if "%OPTION%"=="4" (
     goto startAcestream
 )
 :: Si se introduce una opcion no valida, informa al usuario y regresa al menu.
-echo Opcion no valida. Por favor, intenta de nuevo.
+echo Opcion no valida, por favor intenta nuevamente.
 pause
 goto menu
 
 
 :installDocker
 echo.
-echo Abriendo la pagina oficial de Docker para instalar el software necesario...
+echo [Instalacion de Docker]
+echo ----------------------------------------
+echo Abriendo la pagina oficial de Docker...
+echo Por favor, sigue las instrucciones en el sitio web para completar la instalacion de Docker.
+echo Una vez finalizada la instalacion, regresa a este asistente para continuar.
 start https://www.docker.com/get-started/
-echo Sigue las instrucciones en el sitio web para instalar Docker. Vuelve aqui cuando hayas terminado.
+echo ----------------------------------------
+echo Esperando a que completes la instalacion de Docker...
 pause
 goto menu
 
 
 :configProtocol
 echo.
-echo Configurando el protocolo Acestream en el sistema...
-:: Este paso intenta agregar una clave de registro para verificar si el script tiene privilegios de administrador.
+echo [Configuracion del Protocolo Acestream]
+echo ----------------------------------------
+echo Verificando permisos de administrador para configurar el protocolo Acestream en el sistema...
+
+:: Intenta agregar una clave de registro para verificar si el script tiene privilegios de administrador.
 reg add "HKLM\SOFTWARE\AcestreamSetupCheck" /f >nul 2>&1
 
-:: Si la clave se agrega con exito, el script se esta ejecutando con privilegios de administrador y puede continuar.
 if %errorlevel% == 0 (
+    :: Elimina la clave temporalmente creada, ya que solo se necesita para la comprobacion.
     reg delete "HKLM\SOFTWARE\AcestreamSetupCheck" /f >nul 2>&1
-    echo.
-    echo Estas ejecutando este script como administrador. Continuando...
+    echo Permisos de administrador verificados. Procediendo con la configuracion...
 ) else (
-    echo.
-    echo ERROR: Este proceso requiere permisos de administrador. Por favor, ejecutalo de nuevo como administrador.
+    echo ERROR: Se requieren permisos de administrador para continuar.
+    echo Por favor, cierra esta ventana y ejecuta el script nuevamente como administrador.
     pause
     goto menu
 )
 
-echo.
-:: Agrega entradas al registro de Windows para el protocolo acestream.
+echo Configurando entradas en el registro para el protocolo Acestream...
+:: Agrega las entradas necesarias al registro de Windows.
 reg add "HKEY_CLASSES_ROOT\acestream" /ve /t REG_SZ /d "URL:acestream Protocol" /f
 reg add "HKEY_CLASSES_ROOT\acestream" /v "URL Protocol" /t REG_SZ /d "" /f
-:: Asociacion del protocolo acestream con start_acestream.bat
+:: Asociacion del protocolo Acestream con este archivo SetupAcestream.bat
 reg add "HKEY_CLASSES_ROOT\acestream\shell\open\command" /ve /t REG_SZ /d "\"%~f0\" \"%%1\"" /f
-echo Registros para el protocolo acestream añadidos satisfactoriamente.
 
-:: Verifica que el registro se haya agregado correctamente.
-if %errorlevel% neq 0 (
-    echo.
-    echo No se pudo configurar el protocolo acestream. Asegurate de tener permisos adecuados.
+echo Verificando la configuracion del protocolo...
+:: Verifica que las entradas se hayan agregado correctamente al registro.
+if %errorlevel% == 0 (
+    echo Configuracion del protocolo Acestream completada satisfactoriamente.
+) else (
+    echo Fallo al configurar el protocolo Acestream. Asegurate de tener los permisos adecuados.
     pause
     goto menu
 )
 
-echo.
-echo El protocolo acestream se ha configurado con exito. Reinicia tu navegador para que los cambios surtan efecto.
+echo ----------------------------------------
 pause
 goto menu
 
 
 :installAcestream
 echo.
-echo Preparando la instalacion de Acestream en Docker...
+echo [Instalacion de Acestream en Docker]
+echo ----------------------------------------
+echo Configurando el entorno para Acestream...
 set IMAGE_NAME=smarquezp/docker-acestream-ubuntu-home:latest
 set CONTAINER_NAME=acestream-home-container
 set PORT=6878
 
-echo.
-echo Verificando si Docker esta instalado y en ejecucion...
+echo Verificando Docker...
+:: Verifica si Docker esta instalado y en ejecucion.
 docker --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo ERROR: No se encontro Docker. Instala Docker para poder continuar.
+    echo ERROR: Docker no encontrado. Por favor, instala Docker para continuar.
     start https://www.docker.com/get-started/
     pause
     goto menu
@@ -142,75 +153,68 @@ if %errorlevel% neq 0 (
 
 docker info >nul 2>&1
 if %errorlevel% neq 0 (
-    echo ERROR: Docker no esta funcionando. Por favor, inicia Docker antes de proceder.
+    echo ERROR: Docker no esta activo. Por favor, inicia Docker y vuelve a intentarlo.
     pause
     goto menu
 )
-echo Docker esta listo y funcionando.
+echo Docker verificado con exito y listo para su uso.
 
-echo.
-echo Descargando la imagen mas reciente para Acestream desde Docker Hub...
+echo Descargando la imagen de Acestream...
+:: Descarga la ultima imagen de Acestream desde Docker Hub.
 docker pull %IMAGE_NAME% >nul 2>&1
 if %errorlevel% neq 0 (
-    echo ERROR: No se podido descargar la imagen. Inténtalo de nuevo.
+    echo ERROR: Falla al descargar la imagen de Acestream. Por favor, verifica tu conexion e intentalo nuevamente.
     pause
     goto menu
 )
 
-echo.
-echo Comprobando si ya existe un contenedor con el nombre "%CONTAINER_NAME%"...
-echo.
+echo Verificando contenedores existentes...
+:: Verifica y elimina el contenedor existente si es necesario.
 docker ps -a --filter "name=%CONTAINER_NAME%" --format "{{.Names}}" | findstr /c:"%CONTAINER_NAME%" >nul 2>&1
 if %errorlevel% neq 0 (
     echo Creando un nuevo contenedor para Acestream...
 ) else (
-    echo Un contenedor existente fue encontrado. Limpiando antes de la nueva instalacion...
+    echo Contenedor existente detectado. Eliminando para una nueva instalacion...
     docker rm -f %CONTAINER_NAME% >nul 2>&1
 )
 
 echo.
-:: Verificacion para el usuario, donde podra cambiar la IP interna.
-echo Su IP interna asignada es %INTERNAL_IP%
-echo.
-echo Puedes revisar si es la IP correcta y sobreescribirla en el caso de que no lo fuese a continuacion.
-echo Te dejo un enlace de interes donde puedes ver como obtenerla: https://www.avast.com/es-es/c-how-to-find-ip-address
-pause
-echo.
-echo Si la IP es correcta ^(%INTERNAL_IP%^), presiona ENTER directamente. De lo contrario, escribe la IP correcta y luego presiona ENTER.
-set /p USER_IP=Introduce aqui la IP o pulsa ENTER:
+echo Verificacion de la direccion IP interna...
+:: Permite al usuario confirmar o cambiar la IP interna del contenedor.
+echo Tu direccion IP interna actual es: %INTERNAL_IP%
+echo Si esta es correcta, presiona ENTER. Si no, ingresa la IP correcta y presiona ENTER.
+set /p USER_IP=Introduce la IP o presiona ENTER:
 if not "%USER_IP%"=="" set "INTERNAL_IP=%USER_IP%"
 
-echo.
-echo La IP a utilizar sera: %INTERNAL_IP%
+echo Usando IP: %INTERNAL_IP%
 pause
 
-echo.
-echo Iniciando el contenedor de Acestream con Docker...
+echo Iniciando el contenedor de Acestream...
+:: Inicia el contenedor Docker con Acestream.
 docker run -d -p %PORT%:%PORT% -e INTERNAL_IP=%INTERNAL_IP% --name %CONTAINER_NAME% %IMAGE_NAME% >nul 2>&1
 if %errorlevel% neq 0 (
-    echo ERROR: No se ha podido iniciar el contenedor. Inténtalo de nuevo.
+    echo ERROR: No se pudo iniciar el contenedor. Verifica la configuracion e intentalo de nuevo.
     pause
     goto menu
 )
 
-echo.
-echo El contenedor de Acestream ha sido iniciado con exito y esta escuchando en el puerto %PORT%.
-echo.
+echo Contenedor de Acestream iniciado con exito en el puerto: %PORT%
+echo ----------------------------------------
 goto startAcestream
 
 :startAcestream
 echo.
-echo El navegador se abrira en 5 segundos para reproducir el contenido.
+echo [Reproduccion de Contenido Acestream]
+echo ----------------------------------------
+echo El navegador se abrira en 5 segundos para reproducir el contenido seleccionado.
 timeout /t 5 /nobreak >nul
 
-:: Valida que se ha introducido un ID de stream y procede a abrirlo.
-echo Preparando para abrir el stream Acestream...
+echo Preparando la reproduccion del stream Acestream...
 if not "%STREAM_ID%"=="" (
-    echo Iniciando el stream Acestream con ID: %STREAM_ID%...
+    echo Iniciando stream Acestream con ID: %STREAM_ID%...
     start http://%INTERNAL_IP%:%PORT%/webui/player/%STREAM_ID%
 ) else (
-    echo No se ha proporcionado un ID de stream.
-    echo Abriendo la interfaz web de Acestream para que puedas introducir el ID manualmente...
+    echo Por favor, introduce el ID de stream en la interfaz web de Acestream.
     start http://%INTERNAL_IP%:%PORT%/webui/player/
 )
 pause
@@ -218,14 +222,15 @@ goto exit
 
 :end
 echo.
-echo ==============================================================
-echo Gracias por utilizar mi asistente de configuracion de Acestream x Docker.
-echo Hasta luego!
-echo ==============================================================
-echo.
+echo [Despedida]
+echo -------------------------------------------------------------
+echo Gracias por utilizar el asistente de configuracion de Acestream x Docker.
+echo ¡Esperamos que disfrutes de una excelente experiencia de streaming!
+echo @marquezpsergio
+echo -------------------------------------------------------------
 pause
 
 :exit
-exit /b :: Sale del script de batch limpiamente, devolviendo el control al sistema operativo.
-ENDLOCAL :: Finaliza la localizacion de cambios en el entorno, restaurando los valores originales.
-
+echo Finalizando el script y restaurando el entorno...
+exit /b
+ENDLOCAL
