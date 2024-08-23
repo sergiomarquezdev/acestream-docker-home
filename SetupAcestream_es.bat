0b74b83d5c455931c32d5bb01a946213a4d0d045
Sergio Márquez <sergiomarquezdev@gmail.com>
2024-08-23 14:23:33 +0200

    feat: setupacestream es updated

--- SetupAcestream_es.bat
+++ SetupAcestream_es.bat
@@ -6,10 +6,12 @@ SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 :: -------------------------
 set "IMAGE_NAME=smarquezp/docker-acestream-ubuntu-home:latest"
 set "INTERNAL_IP=127.0.0.1"
-set "PORT=6878"
-set "SERVICE_NAME=acestream"
+set "PORT_BASE=6878"
+set "SERVICE_NAME_BASE=acestream_"
 set "DOCKER_COMPOSE_FILE=docker-compose.yml"
 set "PREFIX=acestream://"
+set "HTTP_PORT_BASE=6878"
+set "HTTPS_PORT_BASE=6879"
 
 :: -------------------------
 :: Verificacion de la instalacion y estado de funcionamiento de Docker.
@@ -56,24 +58,51 @@ if not "%USER_IP%"=="" set "INTERNAL_IP=%USER_IP%"
 echo Usando IP: %INTERNAL_IP%
 
 :: -------------------------
+:: Asignacion dinamica de puertos y nombre del servicio.
+:: -------------------------
+set "PORT=%PORT_BASE%"
+set "SERVICE_NAME=%SERVICE_NAME_BASE%%PORT%"
+set "HTTP_PORT=%HTTP_PORT_BASE%"
+set "HTTPS_PORT=%HTTPS_PORT_BASE%"
+
+:checkPort
+set CONTAINER_ID=
+for /f "tokens=*" %%i in ('docker ps -q --filter "name=!SERVICE_NAME!"') do set CONTAINER_ID=%%i
+
+if not defined CONTAINER_ID (
+    set /a "HTTPS_PORT+=1"
+    echo Usando el puerto !PORT!, puerto HTTP !HTTP_PORT!, puerto HTTPS !HTTPS_PORT!, y el nombre del servicio !SERVICE_NAME!.
+) else (
+    echo El puerto !PORT! ya esta en uso. Probando con el siguiente puerto...
+    set /a "PORT+=2"
+    set /a "HTTP_PORT+=2"
+    set /a "HTTPS_PORT+=2"
+    set "SERVICE_NAME=%SERVICE_NAME_BASE%!PORT!"
+    echo DEBUG: Ahora usando el puerto !PORT!, puerto HTTP !HTTP_PORT!, puerto HTTPS !HTTPS_PORT!, y el nombre del servicio !SERVICE_NAME!.
+    goto checkPort
+)
+
+:: -------------------------
 :: Creacion o actualizacion del archivo docker-compose.yml.
 :: -------------------------
 :startDocker
-docker stop %SERVICE_NAME% >NUL 2>&1
-docker rm %SERVICE_NAME% -f >NUL 2>&1
+docker stop !SERVICE_NAME! >NUL 2>&1
+docker rm !SERVICE_NAME! -f >NUL 2>&1
 echo.
 echo Creando o actualizando el archivo docker-compose.yml...
 >%DOCKER_COMPOSE_FILE% (
     echo version: '3.8'
     echo services:
-    echo   acestream:
-    echo     image: %IMAGE_NAME%
-    echo     container_name: acestream
+    echo   !SERVICE_NAME!:
+    echo     image: !IMAGE_NAME!
+    echo     container_name: !SERVICE_NAME!
     echo     restart: unless-stopped
     echo     ports:
-    echo       - %PORT%:%PORT%
+    echo       - !PORT!:!PORT!
     echo     environment:
-    echo       - INTERNAL_IP=%INTERNAL_IP%
+    echo       - INTERNAL_IP=!INTERNAL_IP!
+    echo       - HTTP_PORT=!HTTP_PORT!
+    echo       - HTTPS_PORT=!HTTPS_PORT!
     echo networks:
     echo   default:
     echo     driver: bridge
@@ -83,18 +112,18 @@ echo Archivo docker-compose.yml creado o actualizado exitosamente.
 
 :: Descarga la imagen Docker mas actualizada antes de arrancar el servicio
 echo Descargando la imagen Docker mas actualizada...
-docker-compose -f %DOCKER_COMPOSE_FILE% pull %SERVICE_NAME%
+docker-compose -f !DOCKER_COMPOSE_FILE! pull !SERVICE_NAME!
 
 :: Intento de iniciar el servicio y manejo de errores en caso de fallo.
 echo Iniciando el servicio de Acestream...
-docker-compose -f %DOCKER_COMPOSE_FILE% up -d %SERVICE_NAME% || (
+docker-compose -f !DOCKER_COMPOSE_FILE! up -d !SERVICE_NAME! || (
     echo ERROR: No se pudo iniciar el servicio Acestream. Asegurate de que el archivo 'docker-compose.yml' este configurado correctamente.
     pause
     goto startDocker
 )
 echo Servicio Acestream iniciado correctamente.
 
-echo Contenedor de Acestream iniciado con exito en el puerto: %PORT%
+echo Contenedor de Acestream iniciado con exito en el puerto: !PORT! usando el puerto HTTP interno: !HTTP_PORT!.
 echo.
 
 :: -------------------------
@@ -105,7 +134,7 @@ echo ----------------------------------------
 echo El navegador se abrira en 5 segundos para reproducir el contenido seleccionado.
 timeout /t 5 /nobreak >nul
 echo Preparando la reproduccion del stream Acestream...
-start http://%INTERNAL_IP%:%PORT%/webui/player/
+start http://!INTERNAL_IP!:!PORT!/webui/player/
 
 :: -------------------------
 :: Mensaje de despedida y finalizacion del script.
